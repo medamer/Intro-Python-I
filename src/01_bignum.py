01d65025b677ea805841e9357d9826c91677c988
Mohamed Edamer <edamer.mo@gmail.com>
2020-08-31 16:07:42 -0700

    today's assg

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,10 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+py = pow(2, 65536)
+print(py)
+#the output for python is infinit numbers
+
+# console.log(math.pow(2, 65536));
+# the output : Infinity
--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,8 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+"%s %s and %s."%(z,x,y)
 # Use the 'format' string method to print the same thing
-
+print("{} {} and {}".format(z,x,y))
 # Finally, print the same thing using an f-string
+print(f"{z} {x} and {y}")

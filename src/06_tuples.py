d56539f8f03b29850bc878d5b188b0c9a41920e2
Mohamed Edamer <edamer.mo@gmail.com>
2020-09-01 08:00:08 -0700

    assignment

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -22,23 +22,27 @@ def dist(a, b):
     x0, y0 = a  # Destructuring assignment
     x1, y1 = b
 
-    return math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
+    print(math.sqrt((x1 - x0)**2 + (y1 - y0)**2))
 
 a = (2, 7)   # <-- x,y coordinates stored in tuples
 b = (-14, 72)
 
 # Prints "Distance is 66.94"
-print("Distance is: {:.2f}".format(dist(a, b)))
+dis = dist(a, b)
+print("Distance is: {:.2f}".format(dis)
 
 
 
 # Write a function `print_tuple` that prints all the values in a tuple
 
 # YOUR CODE HERE
+tup = (1, 2, 5, 7, 99);
 
-t = (1, 2, 5, 7, 99)
-print_tuple(t)  # Prints 1 2 5 7 99, one per line
+def print_tuple(t):
+    for i in t:
+        print(t[i],'\n')
+print_tuple(tup)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
-print_tuple(u)
+u = (1,)  # What needs to be added to make this work?
+print(u)
